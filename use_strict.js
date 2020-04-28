d1402ba133dd0d9d13e46162c99bdeab080a8583
Sushant <sush1393@gmail.com>
2020-04-28 23:34:10 +0530

    Added a comment for explaination

--- use_strict.js
+++ use_strict.js
@@ -5,6 +5,9 @@ function func() {
 func(); // This will return '50' even though height isn't defined anywhere as JavaScript itself creates variables 
         // which are assigned values
 
+
+// One way to fix the above loophole is to use the strict mode as shown below
+
 'use strict'
 function func() {
     height = 50;
