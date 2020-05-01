3247b021058642429df8a4fc3d1243759329df94
Sushant <sush1393@gmail.com>
2020-05-01 12:17:37 +0530

    Scope Example 1

---
+++ scope_example1.js
@@ -0,0 +1,28 @@
+const number = 100
+const string = "Jay"
+let obj1 = {
+  value: "a"
+}
+let obj2 = {
+  value: "b"
+}
+
+let obj3 = obj2;
+ 
+function change(num, str, object1, object2) {
+    num = num * 10;
+    str = "Pete";
+    object1 = object2;
+    object2.value = "c";
+    console.log("Printed from inside function");
+    console.log(num); 
+    console.log(str);
+    console.log(object1.value);
+    console.log(object2.value);
+}
+ 
+change(number, string, obj1, obj2);
+console.log("Printed from outside function");
+console.log(number); 
+console.log(string);
+console.log(obj1.value);
