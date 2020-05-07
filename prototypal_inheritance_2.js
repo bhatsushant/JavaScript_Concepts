d2a9943e2890cefaf9a998b44f7bae71137d6057
Sushant <sush1393@gmail.com>
2020-05-07 12:24:39 +0530

    Add/Modify built-in JavaScript methods

---
+++ prototypal_inheritance_2.js
@@ -0,0 +1,27 @@
+// Adding new methods to built-in functions and modifying existing methods
+
+// Example 1 - Adding a new method, lastYear, to Date function
+
+
+Date.prototype.lastYear = function() {
+    return this.getFullYear - 1;
+}
+
+new Date().lastYear();
+
+/* Current year is 2020 
+Output - 2019 */
+
+new Date('2016').lastYear();
+
+// Output - 2015
+
+// Example 2 - Modifying the map() function of an array
+
+Array.prototype.map = function() {
+    let arr = [];
+    for(let i=0; i<this.length; i++) {
+        arr.push(this[i] + ' MAP');
+    }
+    return arr;
+}
